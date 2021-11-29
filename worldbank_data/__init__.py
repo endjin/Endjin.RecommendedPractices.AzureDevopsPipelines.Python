051c57dcad3346385677a7ab63d80d90bec89a73
ElisendaGascon <elisendagascon@icloud.com>
2021-11-29 12:02:32 +0000

    Create new files for world bank data and notebook

---
+++ worldbank_data/__init__.py
@@ -0,0 +1,1 @@
+from .data_wrangler import DataWrangler  # noqa F401
---
+++ worldbank_data/data_wrangler.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from pathlib import Path
+import csv
+    
+class DataWrangler:
+    def __init__(
+        self,
+        input_folder_path="./data_input",
+        output_folder_path="./data_output",
+        # master_data_file_path="./data_input/master_data/master_data.xlsx",,
+    ):
+
+        self.input_folder_path = Path(input_folder_path)
+        self.output_folder_path = Path(output_folder_path)
+
+
+    def load_raw_data_from_excel(self, filename):
+        return pd.read_csv(
+            Path(self.input_folder_path, filename))
+
+    def unpivot_data(self, df):
+
+        # var_columns = self.get_list_of_data_columns(df)
+        id_vars = ['Country Name', 'Country Code', 'Series Name', 'Series Code']
+        var_name = 'Year'
+        value_name = 'Metric'
+
+        return df.melt(
+            id_vars=id_vars,
+            var_name=var_name,
+            value_name=value_name
+        )
+
+    def clean_years(self, df):
+        df['Year'] = df['Year'].str[:4]
+        return df
